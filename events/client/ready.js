1e378ca0233ea89e2be0520f87cf16bd1f9e919b
LoneWolf6969 <76170076+LoneWolf6969@users.noreply.github.com>
2021-09-19 16:47:59 +0530

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -6,15 +6,7 @@ try {
     } catch (e) {
       client.logger.error(e)
     }
-  let terminate = "1632119948000";
-let date = Date.now()
-  if (date < terminate){
-const ms = terminate-date
-setTimeout(() =>{
-  process.exit()
-},ms)
-  }
-  if(date >= terminate) process.exit();
+  
   //log
 client.logger.ready("Successfully initalised the bot")
   
